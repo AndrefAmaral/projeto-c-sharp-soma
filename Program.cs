1e0060e10b82d62f50a8042195cb7cd14af89b69
Andre Felipe <andrefelipe.pi2@gmail.com>
2023-02-15 16:23:18 -0300

    Projeto soma

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois numeros: ");
+            int x = int.Parse (Console.ReadLine());
+            int y = int.Parse (Console.ReadLine());
+
+            int soma = x + y;
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
